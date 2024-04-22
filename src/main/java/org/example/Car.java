b241c15435b6134bbd9866b793a9dec8168f27de
raveendrahennyake <raveendralakshan5@gmail.com>
2024-04-23 00:19:06 +0530

    git commit -m "messagecutomer"

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,20 +1,46 @@
 package org.example;
 
 public class Car {
-    String Carid;
-    String CarBrand;
-    String Carmode;
-   String Perdayprice;
-    boolean isavaliable;
+     private String Carid;
+    private String CarBrand;
+    private String Carmode;
+    private int Perdayprice;
+    private boolean isavaliable;
 
-    public Car (String Carid,String CarBrand,String Carmode,String Perdayprice,boolean isavaliable) {
+    public Car (String Carid,String CarBrand,String Carmode,int Perdayprice) {
         this.Carid=Carid;
         this.CarBrand=CarBrand;
         this.Carmode=Carmode;
         this.Perdayprice=Perdayprice;
-        this.isavaliable=isavaliable;
+        this.isavaliable=true;
+    }
+    public String getCarid () {
+        return Carid;
+    }
+
+    public String getCarBrand () {
+        return CarBrand;
+    }
+
+    public String getCarmode () {
+        return Carmode;
+    }
+    public int getPerdayprice () {
+        return Perdayprice;
+    }
+    public double calulatePrice (int rentalDays) {
+        return rentalDays*Perdayprice;
 
     }
+    public boolean isavailable() {
+        return isavaliable;
+    }
+
+
+
+
+
+
 
     }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,14 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-       Car obj1=new Car("CC1","BMW","X5","10$",true);
-       Car obj2=new Car("CC2","AUDI","Y5","15$",true);
+       Car obj1=new Car("CC1","BMW","X5",10);
+       Car obj2=new Car("CC2","AUDI","Y5",15);
+       Car obj3=new Car("CC3","M-BENZ","Z5",20);
+
+//        System.out.println(obj1.getCarid());
+        System.out.println(obj2.getCarid());
+
+
 
 
 
